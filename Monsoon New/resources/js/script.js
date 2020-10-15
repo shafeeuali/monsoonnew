49cc21e5534e0655f6b5fbb6e0bdc5f5ec78fa59
shafeeuali <37484114+shafeeuali@users.noreply.github.com>
2020-10-15 10:21:39 +0500

    Add files via upload

---
+++ Monsoon New/resources/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+	
+	$('.js--section-features').waypoint(function(direction){
+		if(direction === "down") {
+			$('nav').addClass('sticky');
+		} else {
+			$('nav').removeClass('sticky');
+		}
+	}, {
+ 		offset: '50%'
+});
+	
+	// var waypoints = $('#handler-first').waypoint(function(direction) {
+//  notify(this.element.id + ' hit 25% from top of window') 
+//}, {
+//  offset: '25%'
+//})
+});
+
+
+
+
